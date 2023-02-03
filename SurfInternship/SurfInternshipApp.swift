65e22841458152179e010ec52aa5b0689e5df9bc
dolludaa <dolontaeva00@gmail.com>
2023-02-03 10:59:27 +0300

    Xcode project created

---
+++ SurfInternship/SurfInternshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfInternshipApp.swift
+//  SurfInternship
+//
+//  Created by Людмила Долонтаева on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SurfInternshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
